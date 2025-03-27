ebed8a87b3650c87a3ee320d5426c666a209e141
zealleaf <github@zealleaf.me>
2025-03-27 21:47:42 +0800

    feat(plugins): replace bufferline and snacks-explorer with neo-tree

--- lua/plugins/incline.nvim.lua
+++ lua/plugins/incline.nvim.lua
@@ -1,12 +1,30 @@
 return {
   {
     "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
     config = function()
+      local colors = require("solarized-osaka.colors").setup()
       require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
         window = { margin = { vertical = 0, horizontal = 0 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          return { { filename } }
+        end,
       })
     end,
-    -- Optional: Lazy load Incline
-    event = "VeryLazy",
   },
 }
