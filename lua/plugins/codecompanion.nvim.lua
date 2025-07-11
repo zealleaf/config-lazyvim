c82392a325386652ea40579efc5dde537f7b1701
zealleaf <github@zealleaf.me>
2025-07-11 21:22:43 +0800

    refactor(codecompanion): comment out unused extensions configuration

--- lua/plugins/codecompanion.nvim.lua
+++ lua/plugins/codecompanion.nvim.lua
@@ -80,45 +80,45 @@ return {
         },
       },
     },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          show_result_in_chat = true,
-          make_vars = true,
-          make_slash_commands = true,
-        },
-      },
-      vectorcode = {
-        opts = {
-          tool_group = {
-            enabled = true,
-            extras = {},
-            collapse = false,
-          },
-          tool_opts = {
-            ["*"] = {},
-            ls = {},
-            vectorise = {},
-            query = {
-              max_num = { chunk = -1, document = -1 },
-              default_num = { chunk = 50, document = 10 },
-              include_stderr = false,
-              use_lsp = false,
-              no_duplicate = true,
-              chunk_mode = false,
-              summarise = {
-                enabled = false,
-                adapter = nil,
-                query_augmented = true,
-              },
-            },
-            files_ls = {},
-            files_rm = {},
-          },
-        },
-      },
-    },
+    -- extensions = {
+    --   mcphub = {
+    --     callback = "mcphub.extensions.codecompanion",
+    --     opts = {
+    --       show_result_in_chat = true,
+    --       make_vars = true,
+    --       make_slash_commands = true,
+    --     },
+    --   },
+    --   vectorcode = {
+    --     opts = {
+    --       tool_group = {
+    --         enabled = true,
+    --         extras = {},
+    --         collapse = false,
+    --       },
+    --       tool_opts = {
+    --         ["*"] = {},
+    --         ls = {},
+    --         vectorise = {},
+    --         query = {
+    --           max_num = { chunk = -1, document = -1 },
+    --           default_num = { chunk = 50, document = 10 },
+    --           include_stderr = false,
+    --           use_lsp = false,
+    --           no_duplicate = true,
+    --           chunk_mode = false,
+    --           summarise = {
+    --             enabled = false,
+    --             adapter = nil,
+    --             query_augmented = true,
+    --           },
+    --         },
+    --         files_ls = {},
+    --         files_rm = {},
+    --       },
+    --     },
+    --   },
+    -- },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
